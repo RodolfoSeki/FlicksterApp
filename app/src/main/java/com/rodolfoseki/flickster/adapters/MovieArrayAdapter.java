606cba231bd7b57d3d5f6c57a7b9cad73d4eb6b3
Rodolfo Sekijima <rodolfoseki@gmail.com>
2016-10-09 17:09:37 -0300

    Required functionalities added

---
+++ app/src/main/java/com/rodolfoseki/flickster/adapters/MovieArrayAdapter.java
@@ -0,0 +1,64 @@
+package com.rodolfoseki.flickster.adapters;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.rodolfoseki.flickster.R;
+import com.rodolfoseki.flickster.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by spread on 09/10/2016.
+ */
+
+public class MovieArrayAdapter extends ArrayAdapter<Movie>  {
+
+    public MovieArrayAdapter(Context context, List<Movie> movies){
+        super(context, android.R.layout.simple_list_item_1 , movies);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        int orientation = getContext().getResources().getConfiguration().orientation;
+
+        //get the data item for position
+        Movie movie = getItem(position);
+
+        //check the existing view being reused
+        if (convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        // find image view
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+        // clear out image from convertView
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        // populate data
+        tvTitle.setText(movie.getOrigialTitle());
+        tvOverview.setText(movie.getOverview());
+
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Picasso.with(getContext()).load(movie.getBackdropPath()).into(ivImage);
+        }
+        // return the view
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/rodolfoseki/flickster/models/Movie.java
@@ -0,0 +1,57 @@
+package com.rodolfoseki.flickster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by spread on 06/10/2016.
+ */
+
+public class Movie {
+
+
+    String posterPath;
+    String backdropPath;
+    String origialTitle;
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException{
+        this.posterPath = jsonObject.getString("poster_path");
+        this.backdropPath = jsonObject.getString("backdrop_path");
+        this.origialTitle = jsonObject.getString("original_title");
+        this.overview = jsonObject.getString("overview");
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342%s",posterPath);
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342%s",backdropPath);
+    }
+
+    public String getOrigialTitle() {
+        return origialTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public static ArrayList<Movie> fromJSONArray(JSONArray array){
+        ArrayList<Movie> results = new ArrayList<>();
+
+        for(int x = 0; x < array.length();x++){
+            try {
+                results.add(new Movie(array.getJSONObject(x)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return results;
+    }
+}
